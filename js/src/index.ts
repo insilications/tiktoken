46f51fa0de5030ff2eeb395693f53b2ae9f32942
Mikola Lysenko <mikolalysenko@gmail.com>
2024-04-19 22:29:12 +0100

    handle empty input case

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,9 +1,9 @@
 import { TiktokenModel, TiktokenEncoding } from "./ranks/ranks";
-import gpt2 from "./ranks/gpt2";
-import p50k_base from "./ranks/p50k_base";
-import p50k_edit from "./ranks/p50k_edit";
-import r50k_base from "./ranks/r50k_base";
-import cl100k_base from "./ranks/cl100k_base";
+// import gpt2 from "./ranks/gpt2";
+// import p50k_base from "./ranks/p50k_base";
+// import p50k_edit from "./ranks/p50k_edit";
+// import r50k_base from "./ranks/r50k_base";
+// import cl100k_base from "./ranks/cl100k_base";
 import o200k_base from "./ranks/o200k_base";
 
 import { Tiktoken, getEncodingNameForModel } from "./core";
@@ -14,20 +14,20 @@ export function getEncoding(
   extendSpecialTokens?: Record<string, number>
 ): Tiktoken {
   switch (encoding) {
-    case "gpt2":
-      return new Tiktoken(gpt2, extendSpecialTokens);
-    case "r50k_base":
-      return new Tiktoken(r50k_base, extendSpecialTokens);
-    case "p50k_base":
-      return new Tiktoken(p50k_base, extendSpecialTokens);
-    case "p50k_edit":
-      return new Tiktoken(p50k_edit, extendSpecialTokens);
-    case "cl100k_base":
-      return new Tiktoken(cl100k_base, extendSpecialTokens);
+    // case "gpt2":
+    //   return new Tiktoken(gpt2, extendSpecialTokens);
+    // case "r50k_base":
+    //   return new Tiktoken(r50k_base, extendSpecialTokens);
+    // case "p50k_base":
+    //   return new Tiktoken(p50k_base, extendSpecialTokens);
+    // case "p50k_edit":
+    //   return new Tiktoken(p50k_edit, extendSpecialTokens);
+    // case "cl100k_base":
+    //   return new Tiktoken(cl100k_base, extendSpecialTokens);
     case "o200k_base":
       return new Tiktoken(o200k_base, extendSpecialTokens);
     default:
-      never(encoding);
+    //   never(encoding);
       throw new Error("Unknown encoding");
   }
 }
--- js/webpack.config.js
+++ js/webpack.config.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  entry: './src/lite.ts',
+  entry: './src/index.ts',
   module: {
     rules: [
       {
